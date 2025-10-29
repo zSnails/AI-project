e40d113a93841477b77cbe5b223dbd4c574a2e26
zSnails <erizojuan33@gmail.com>
2025-10-29 11:27:30 -0600

    feat(WIP): add static route for vue frontend

--- server.py
+++ server.py
@@ -31,13 +31,20 @@ PERSON_GROUP_ID = id if id != "" else str(uuid4())
 face_client = FaceClient(endpoint=ENDPOINT, credential=AzureKeyCredential(API_KEY))
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="./static")
 
 app.config["UPLOAD_FOLDER"] = "./uploads/"
 app.config["RESULTS_FOLDER"] = "./detection_results/"
 
 
 bike_toll_model: RandomForestRegressor = load("./models/bike_price/bike_ride_price.model.pkl")
+
+
+@app.route("/", methods=["GET"])
+def serve_vue_frontend():
+    if app.static_folder:
+        return send_from_directory(app.static_folder, "index.html")
+    return jsonify({"code": 500, "message": "Internal Server Error"}), 500
 
 
 @app.route("/api/models/bike-toll", methods=["GET"])
